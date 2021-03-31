ceffecf1aa9e2743f18049e66c5cce662b162e7d
Anton Dubynin <anton.dubynin@globallogic.com>
2021-03-31 17:27:58 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# web-socket-alerts
+### Purpose
+
+Demonstrate how web sockets can be used to push messages from back-end to user agent.
+
+### Prerequisite
+
+node.js 10+
+
+### Usage
+
+1. `npm i` - install dependencies
+1. `npm start` - start server locally
+1. open `localhost:8080` in browser - WS connection will be registered by server
+1. use any suitable tool to make POST request to `localhost:8080/msg` API
+
+Request body:
+`{ id: 'client_id', msg: 'message' }`
+
+Example:
+
+`curl -X POST -d '{ "id": "9dd005ba-f650", "msg": "Hello World!" }' -H 'Content-Type: application/json' 127.0.0.1:8080/msg`
+
+### Useful links
+
+https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
---
+++ server.js
@@ -0,0 +1,71 @@
+const webSocketServer = require('websocket').server;
+const http = require('http');
+const fs = require('fs').promises;
+
+// map which contains all established web socket connections
+const authorizedConnections = {};
+
+// simple request handler
+const listener = async (req, res) => {
+    switch (req.url) {
+        case '/':
+            const html = await fs.readFile('index.html');
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.write(html);
+            res.end();
+            break;
+        case '/msg':
+            let data = '';
+
+            req.on('data', chunk => {
+                data += chunk;
+            });
+
+            req.on('end', () => {
+                const { id, msg } = JSON.parse(data);
+
+                // if client with given id exists, send message to him using web socket connection
+                if (authorizedConnections.hasOwnProperty(id)) {
+                    authorizedConnections[id].sendUTF(msg);
+                    console.log(`Sent message '${msg}' to client ${id}`);
+                }
+
+                res.writeHead(200);
+                res.end();
+            });
+            break;
+        default:
+            res.writeHead(400);
+            res.end();
+            break;
+    }
+};
+
+const server = http.createServer(listener);
+
+server.listen(8080);
+
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+wsServer.on('request', request => {
+    const connection = request.accept(null, request.origin);
+
+    connection.on('message', ({ utf8Data }) => {
+        // utf8Data - user token or id sent from front-end
+        // replace next line with auth logic (find user id DB, check permissions etc.)
+        const userId = utf8Data;
+
+        if (!authorizedConnections.hasOwnProperty(userId)) {
+            authorizedConnections[userId] = connection;
+            console.log(`connected: ${userId}`);
+            console.log(`clients list: ${Object.getOwnPropertyNames(authorizedConnections)}`);
+
+            connection.on('close', () => {
+                delete authorizedConnections[userId];
+                console.log(`disconnected: ${userId}`);
+            });
+        }
+    });
+});
